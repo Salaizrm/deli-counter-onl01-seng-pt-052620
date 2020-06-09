de3600a6fc59f0dbef6c2e8317b4aa51227617a1
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-09 17:44:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def line(x)
   if x.empty?
     puts "The line is currently empty."
@@ -22,4 +23,22 @@ def now_serving(array)
     puts "Currently serving #{array.first}."
     array.shift
   end
+=======
+class deli
+
+  @@all = []
+
+def line
+  if @@all = []
+    puts "The line is currently empty."
+  else
+    puts "now serving #{}"
+  end
+end
+
+def take_a_number
+end
+
+def now_serving
+>>>>>>> d6f03d4a24a7707f3734f1e47a3d83d6d4cf7ca4
 end
